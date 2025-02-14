9875d5842b93f73f296bcd4364b674b0147c0455
Sovanyuth Yun <120990791+Sovanyuth@users.noreply.github.com>
2025-02-14 10:20:38 -0500

    submission

--- src/movie.cpp
+++ src/movie.cpp
@@ -5,7 +5,6 @@
 using namespace std;
 
 Movie::Movie(string title, string director, int id, int runtime, double rating)
-	:id(id), title(title), director(director), runtime(runtime), rating(rating)
 {
 	// Error 1: Throw an exception when the movie constructor is given at least 1 empty vectors
 	if (title.empty() || director.empty())
@@ -13,12 +12,12 @@ Movie::Movie(string title, string director, int id, int runtime, double rating)
 		throw invalid_argument("Error (Invalid Movie): The title and director should not be empty!");
 	}
 
-	if (id < 0 || id > 10000)
+	if (id < 0)
 	{
 		throw invalid_argument("Error (Invalid ID): Id must not be negative!");
 	}
 
-	if (runtime < 0 || runtime > 51420)
+	if (runtime < 0)
 	{
 		throw invalid_argument("Error (Invalid Runtime): Runtime must not be negative! Also funfact: the longest movie ever made is 857 hours or 51420 minutes");
 	}
@@ -27,6 +26,12 @@ Movie::Movie(string title, string director, int id, int runtime, double rating)
 	{
 		throw invalid_argument("Ratings must be from 0-10");
 	}
+
+	this->title = title;
+	this->director = director;
+	this->id = id;
+	this->runtime = runtime;
+	this->rating = rating;
 }
 
 int Movie::GetId() 
